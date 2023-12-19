f1013f08d72465f52a199d9a5959be42f92aa80d
Georgii <panovgeorgij819@gmail.com>
2023-12-19 14:20:42 +0500

    update

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -1,6 +1,6 @@
 create table if not exists customer
 (
     id_client serial primary key,
-    nick_name text not null unique,
+    nick_name varchar(15) not null unique,
     card_id  text  not null
 )
